d636ec5ee6c29c5f6cde5f0d89b7a631f2c9e0bc
ashmit1795 <ashmitpatra77@gmail.com>
2026-07-01 21:49:28 +0530

    feat: implement basic Express server with a greeting endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
-function greeting(name: string): void {
-    console.log(`Hello, ${name}! Welcome to the PizzaMesh Auth Service.`);
-}
+import app from "./app.ts";
+import { Env } from "./config/env.ts";
 
-greeting("Alice");
+const PORT = Env.PORT || 5000;
+
+const startServer = () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(
+                `Server is running on port ${PORT}. Visit http://localhost:${PORT}/ to access the application.`,
+            );
+        });
+    } catch (error) {
+        console.error("Error starting the server:", error);
+        process.exit(1);
+    }
+};
+
+startServer();
